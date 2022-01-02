c063e968d0cbf2e04c7d686ed22b5ebd25e10377
LeeDongN <ymj07183@naver.com>
2022-01-03 01:07:44 +0900

    GANO

--- js/test_writeInfo.js
+++ js/test_writeInfo.js
@@ -15,6 +15,7 @@ function begin_write() {
     }, 200)
   }, 200);
 };
+
 function ToList() {
   writein.style.WebkitAnimation = "fadeOut 0.5s";
   writein.style.animation = "fadeOut 0.5s";
@@ -31,35 +32,39 @@ function ToList() {
 function New_Info_open() {
   document.getElementById("New_Info").style.display = 'block';
 };
+
 function New_Info_close() {
-  document.getElementById("New_Info").style.display='none';
+  document.getElementById("New_Info").style.display = 'none';
 }
 ///////////////////////////////////////////////////////////////////
 //case1: 시술/수술 클릭 시 열림
 function New_1_open() {
   document.getElementById("New_1").style.display = 'block';
-  document.getElementById("New_Info").style.display='none';
+  document.getElementById("New_Info").style.display = 'none';
 };
+
 function New_1_close() {
-  document.getElementById("New_1").style.display='none';
+  document.getElementById("New_1").style.display = 'none';
   document.getElementById("New_Info").style.display = 'block';
 }
 //case2 질병 악화 누를 시 열림
 function New_2_open() {
   document.getElementById("New_2").style.display = 'block';
-  document.getElementById("New_Info").style.display='none';
+  document.getElementById("New_Info").style.display = 'none';
 };
+
 function New_2_close() {
-  document.getElementById("New_2").style.display='none';
+  document.getElementById("New_2").style.display = 'none';
   document.getElementById("New_Info").style.display = 'block';
 }
 //case3 검사 누를 시 열림
 function New_3_open() {
   document.getElementById("New_3").style.display = 'block';
-  document.getElementById("New_Info").style.display='none';
+  document.getElementById("New_Info").style.display = 'none';
 };
+
 function New_3_close() {
-  document.getElementById("New_3").style.display='none';
+  document.getElementById("New_3").style.display = 'none';
   document.getElementById("New_Info").style.display = 'block';
 }
 
@@ -68,22 +73,25 @@ function New_3_close() {
 function surgery_open() {
   document.getElementById("surgery").style.display = 'block';
 };
+
 function surgery_close() {
-  document.getElementById("surgery").style.display='none';
+  document.getElementById("surgery").style.display = 'none';
 }
 //검사정보 버튼을 눌렀을 때 나오는 창
 function checkup_open() {
   document.getElementById("checkup").style.display = 'block';
 };
+
 function checkup_close() {
-  document.getElementById("checkup").style.display='none';
+  document.getElementById("checkup").style.display = 'none';
 }
 // 증상 관리, 치료 상황을 눌렀을 때 나오는 창
 function care_open() {
   document.getElementById("care").style.display = 'block';
 };
+
 function care_close() {
-  document.getElementById("care").style.display='none';
+  document.getElementById("care").style.display = 'none';
 }
 ///////////////////////////////////////////////////////////////
 // prn 오더 눌럿을 때 나오는 창
@@ -92,36 +100,51 @@ function care_close() {
 function vital_open() {
   document.getElementById("vital").style.display = 'block';
 };
+
 function vital_close() {
-  document.getElementById("vital").style.display='none';
+  document.getElementById("vital").style.display = 'none';
 }
 // 특이사항을 눌렀을 때 나오는 창
 function unusal_open() {
   document.getElementById("unusal").style.display = 'block';
 };
+
 function unusal_close() {
-  document.getElementById("unusal").style.display='none';
+  document.getElementById("unusal").style.display = 'none';
 }
 ///////////////////////////////////////////////////////////////
 $("#info-contents-hidden1").mouseover(function() {
   $("#info-contents-write-add1").fadeIn(100);
-  document.getElementById("info-contents-write-add2").style.display='none';
-  document.getElementById("info-contents-write-add3").style.display='none';
+  document.getElementById("info-contents-write-add2").style.display = 'none';
+  document.getElementById("info-contents-write-add3").style.display = 'none';
 });
 
 $("#info-contents-hidden2").mouseover(function() {
   $("#info-contents-write-add2").fadeIn(100);
-  document.getElementById("info-contents-write-add1").style.display='none';
-  document.getElementById("info-contents-write-add3").style.display='none';
+  document.getElementById("info-contents-write-add1").style.display = 'none';
+  document.getElementById("info-contents-write-add3").style.display = 'none';
 });
 
 $("#info-contents-hidden3").mouseover(function() {
   $("#info-contents-write-add3").fadeIn(100);
-  document.getElementById("info-contents-write-add1").style.display='none';
-  document.getElementById("info-contents-write-add2").style.display='none';
+  document.getElementById("info-contents-write-add1").style.display = 'none';
+  document.getElementById("info-contents-write-add2").style.display = 'none';
 });
 
+$("#info-sidebar-repeat").mouseover(function() {
+  document.getElementById("info-sidebar-repeat-hover").style.display = 'flex';
+});
+$("#info-sidebar-repeat").mouseout(function() {
+  document.getElementById("info-sidebar-repeat-hover").style.display = 'none';
+});
+$("#info-sidebar-special").mouseover(function() {
+  document.getElementById("info-sidebar-special-hover").style.display = 'flex';
+});
+$("#info-sidebar-special").mouseout(function() {
+  document.getElementById("info-sidebar-special-hover").style.display = 'none';
+});
 var sym = 1;
+
 function add_checkup_box1() {
   contents = '';
   var l = 'info-contents-checkup1_' + sym++;
@@ -132,6 +155,7 @@ function add_checkup_box1() {
 
 var checkup = 1;
 var checkup2 = 1;
+
 function add_checkup_box2() {
   contents = '';
   var i = 'str2_' + checkup++;
@@ -145,6 +169,7 @@ function add_checkup_box2() {
 
 var cure1 = 1;
 var cure2 = 1;
+
 function add_checkup_box3() {
   contents = '';
   var i = 'str1_' + cure1++;
@@ -155,3 +180,30 @@ function add_checkup_box3() {
   contents.attr('id', l)
   $('#info-contents-hidden3').append(contents);
 };
+
+var angle_r = 0;
+$('#repeat-icon').click(function() {
+
+  if (angle_r == 0) {
+    $('#repeat-icon').css('transform', 'rotate(90deg)');
+    angle_r++;
+    document.getElementById("repeatHidden").style.display = 'block'
+  } else if (angle_r == 1) {
+    $('#repeat-icon').css('transform', 'rotate(0deg)');
+    angle_r--;
+    document.getElementById("repeatHidden").style.display = 'none'
+  }
+});
+
+var angle_s = 0;
+$('#special-icon').click(function() {
+  if (angle_s == 0) {
+    $('#special-icon').css('transform', 'rotate(90deg)');
+    angle_s++;
+    document.getElementById("specialHidden").style.display = 'block'
+  } else if (angle_s == 1) {
+    $('#special-icon').css('transform', 'rotate(0deg)');
+    angle_s--;
+    document.getElementById("specialHidden").style.display = 'none'
+  }
+});
