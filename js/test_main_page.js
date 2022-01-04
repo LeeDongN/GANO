9a693ec9cf8ed8bd36e1c5ba4d7559250790a737
LeeDongN <ymj07183@naver.com>
2022-01-05 02:07:45 +0900

    GANO-version_1.2

--- js/test_main_page.js
+++ js/test_main_page.js
@@ -8,11 +8,20 @@ function w3_close() {
   document.getElementById("myOverlay").style.display = "none";
 }
 
+$('input[type=number][maxlength]').on('input', function(ev) {
+    var $this = $(this);
+    var maxlength = $this.attr('maxlength');
+    var value = $this.val();
+    if (value && value.length >= maxlength) {
+        $this.val(value.substr(0, maxlength));
+    }
+});
+
+
 // 환자 클릭시 보여지는 팝업 창
 // When the user clicks anywhere outside of the modal, close it
 function PatientsModal_close() {
   document.getElementById('PatientsModal').style.display='none';
-
 }
 
 var global_bedID = {};
@@ -20,7 +29,6 @@ var global_bedID = {};
 function P_plusModal_open(a) {
   document.getElementById("P_plusModal").style.display='block';
   global_bedID = $(a).parent().prop('id');
-  console.log(global_bedID);
 }
 
 function P_plusModal_close() {
@@ -30,6 +38,7 @@ function P_plusModal_close2() {
   document.getElementById("P_plusModal").style.display='none';
   resetForm();
 }
+
 
 //병실 및 인원 설정
 function roomOption_open() {
@@ -54,15 +63,6 @@ function roomAdd() {
   };
 }
 
-$('input[type=number][maxlength]').on('input', function(ev) {
-    var $this = $(this);
-    var maxlength = $this.attr('maxlength');
-    var value = $this.val();
-    if (value && value.length >= maxlength) {
-        $this.val(value.substr(0, maxlength));
-    }
-});
-
 function emptyBedAdd() {
   var empty = document.getElementById("roomOption_person").value;
   for (i=0; i<empty; i++){
@@ -81,7 +81,6 @@ function roomOption_choose() {
   document.getElementById("roomOption").style.display='none';
   document.getElementById("patients_list_start").style.display = 'none';
   document.getElementById("patients_list_roomChoise_frame").style.display='flex';
-  //document.getElementById("roomOption_person").value;
   document.getElementById("roomOption_room").value = "";
   document.getElementById("roomOption_person").value = "";
 }
