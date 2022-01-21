957bc7fbf276eef27d4acf7aeafafc0cb2d2377d
LeeDongN <ymj07183@naver.com>
2022-01-22 01:30:03 +0900

    GANO_version_2.2.1

--- js/writeInfo_Special.js
+++ js/writeInfo_Special.js
@@ -2,3 +2,20 @@ function info_ToSpecial() {
   document.getElementById('info_newPatient').style.display = 'none'
   document.getElementById('info_Special').style.display = 'grid'
 }
+
+function info_test_solve(a) {
+  var k = $(a).parent('div').next().children('div:eq(0)');
+  var css = new Array();
+  css = k.prop('style').cssText;
+  console.log(k.prev());
+
+  if (css[9]=='n') {
+    k.prev().prop('style', 'margin:0px; color:gray; text-decoration:line-through')
+    k.parent().prev().children().prop('class', 'info-Special-wide-title2-solved w3-hover-shadow')
+    k.prop('style', 'display:flex; margin:15px 0px 0px 15px;');
+  } else {
+    k.prev().prop('style', 'margin:0px;')
+    k.parent().prev().children().prop('class', 'info-Special-wide-title2 w3-hover-shadow')
+    k.hide();
+  }
+}
